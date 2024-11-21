1ae7b106efd47379c4538fc813dd2e49c88d19d1
Pedro Murilo <pedromarmindo@gmail.com>
2024-11-21 19:47:35 -0300

    Project created

---
+++ src/main/java/com/grupodaescola/agendamente/AgendamenteApplication.java
@@ -0,0 +1,13 @@
+package com.grupodaescola.agendamente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendamenteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupodaescola/agendamente/AgendamenteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupodaescola.agendamente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamenteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
