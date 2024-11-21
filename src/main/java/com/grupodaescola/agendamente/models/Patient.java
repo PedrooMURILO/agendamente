e0a004e222a89c042dc59f003707340e99855e93
Pedro Murilo <pedromarmindo@gmail.com>
2024-11-21 19:51:03 -0300

    Properties, Patient entity

---
+++ src/main/java/com/grupodaescola/agendamente/models/Patient.java
@@ -0,0 +1,80 @@
+package com.grupodaescola.agendamente.models;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_patient")
+public class Patient {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String name;
+	private String phone;
+	private boolean whatsapp;
+
+	public Patient() {
+	}
+
+	public Patient(Integer id, String name, String phone, boolean whatsapp) {
+		this.id = id;
+		this.name = name;
+		this.phone = phone;
+		this.whatsapp = whatsapp;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public boolean isWhatsapp() {
+		return whatsapp;
+	}
+
+	public void setWhatsapp(boolean whatsapp) {
+		this.whatsapp = whatsapp;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Patient other = (Patient) obj;
+		return Objects.equals(id, other.id);
+	}
+}
