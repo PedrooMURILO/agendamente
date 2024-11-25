503bf53fd87b1dd481b7fe5ed0f8ed1b913690a1
Pedro Murilo <pedromarmindo@gmail.com>
2024-11-25 00:05:55 -0300

    Patient update

--- src/main/java/com/grupodaescola/agendamente/controllers/PatientController.java
+++ src/main/java/com/grupodaescola/agendamente/controllers/PatientController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,5 +47,11 @@ public class PatientController {
 	public ResponseEntity<Object> delete(@PathVariable Integer id) {
 		patientService.delete(id);
 		return ResponseEntity.status(HttpStatus.OK).body("Product deleted successfully.");
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<PatientDTO> update(@PathVariable Integer id, @RequestBody Patient patient) {
+	    PatientDTO updatedPatient = patientService.update(id, patient);
+	    return ResponseEntity.status(HttpStatus.OK).body(updatedPatient);
 	}
 }
--- src/main/java/com/grupodaescola/agendamente/services/PatientService.java
+++ src/main/java/com/grupodaescola/agendamente/services/PatientService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.grupodaescola.agendamente.dtos.PatientDTO;
 import com.grupodaescola.agendamente.models.Patient;
@@ -33,5 +34,18 @@ public class PatientService {
 	
 	public void delete(Integer id) {
 		patientRepository.deleteById(id);
+	}
+	
+	@Transactional
+	public PatientDTO update(Integer id, Patient patient) {
+		Patient existingPatient = patientRepository.getReferenceById(id);
+		updateData(existingPatient, patient);
+		return new PatientDTO(patientRepository.save(existingPatient));
+	}
+	
+	private void updateData(Patient existingPatient, Patient patient) {
+		existingPatient.setName(patient.getName());
+		existingPatient.setPhone(patient.getPhone());
+		existingPatient.setWhatsapp(patient.isWhatsapp());
 	}
 }
