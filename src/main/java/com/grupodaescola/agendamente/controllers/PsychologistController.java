34347f3d092f4fd586bb56f82a12a098a37f46f3
Pedro Murilo <pedromarmindo@gmail.com>
2024-11-28 21:39:35 -0300

    Psychologist getById, insert, delete and update

--- src/main/java/com/grupodaescola/agendamente/controllers/PsychologistController.java
+++ src/main/java/com/grupodaescola/agendamente/controllers/PsychologistController.java
@@ -5,7 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +28,29 @@ public class PsychologistController {
 	public ResponseEntity<List<PsychologistDTO>> findAll() {
 		List<PsychologistDTO> psychologistList = psychologistService.findAll();
 		return ResponseEntity.status(HttpStatus.OK).body(psychologistList);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<PsychologistDTO> findById(@PathVariable Integer id) {
+		PsychologistDTO psychologist = psychologistService.findById(id);
+		return ResponseEntity.status(HttpStatus.OK).body(psychologist);
+	}
+	
+	@PostMapping
+	public ResponseEntity<PsychologistDTO> insert(@RequestBody PsychologistDTO psychologist) {
+		PsychologistDTO savedPsychologist = psychologistService.insert(psychologist);
+		return ResponseEntity.status(HttpStatus.CREATED).body(savedPsychologist);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Object> delete(@PathVariable Integer id) {
+		psychologistService.delete(id);
+		return ResponseEntity.status(HttpStatus.OK).body("Product deleted successfully.");
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<PsychologistDTO> update(@PathVariable Integer id, @RequestBody PsychologistDTO psychologist) {
+	    PsychologistDTO updatedPsychologist = psychologistService.update(id, psychologist);
+	    return ResponseEntity.status(HttpStatus.OK).body(updatedPsychologist);
 	}
 }
--- src/main/java/com/grupodaescola/agendamente/services/PsychologistService.java
+++ src/main/java/com/grupodaescola/agendamente/services/PsychologistService.java
@@ -1,9 +1,12 @@
 package com.grupodaescola.agendamente.services;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.grupodaescola.agendamente.dtos.PsychologistDTO;
 import com.grupodaescola.agendamente.models.Psychologist;
@@ -18,5 +21,38 @@ public class PsychologistService {
 	public List<PsychologistDTO> findAll() {
 		List<Psychologist> psychologistList = psychologistRepository.findAll();
 		return psychologistList.stream().map(x -> new PsychologistDTO(x)).toList();
+	}
+	
+	public PsychologistDTO findById(Integer id) {
+		Optional<Psychologist> psychologist = psychologistRepository.findById(id);
+		return new PsychologistDTO(psychologist.get());
+	}
+	
+	public PsychologistDTO insert(PsychologistDTO psychologist) {
+		Psychologist savedPsychologist = new Psychologist();
+		BeanUtils.copyProperties(psychologist, savedPsychologist);
+		savedPsychologist = psychologistRepository.save(savedPsychologist);
+		return new PsychologistDTO(savedPsychologist);
+	}
+	
+	public void delete(Integer id) {
+		psychologistRepository.deleteById(id);
+	}
+	
+	@Transactional
+	public PsychologistDTO update(Integer id, PsychologistDTO psychologist) {
+		Psychologist existingPsychologist = psychologistRepository.getReferenceById(id);
+		updateData(existingPsychologist, psychologist);
+		return new PsychologistDTO(psychologistRepository.save(existingPsychologist));
+	}
+	
+	private void updateData(Psychologist existingPsychologist, PsychologistDTO psychologist) {
+		existingPsychologist.setName(psychologist.getName());
+		existingPsychologist.setBusinessName(psychologist.getBusinessName());;
+		existingPsychologist.setPhone(psychologist.getPhone());;
+		existingPsychologist.setEmail(psychologist.getEmail());
+		existingPsychologist.setPassword(psychologist.getPassword());
+		existingPsychologist.setProfessionalLicense(psychologist.getProfessionalLicense());
+		
 	}
 }
